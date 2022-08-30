f1011604cab7617bba19430dae87929e4e9abbb4
se0nShine <58286830+se0nShine@users.noreply.github.com>
2022-08-30 13:58:34 +0900

    Add files via upload
    08/30 수정

--- detectLineAngle_rasberry.py
+++ detectLineAngle_rasberry.py
@@ -1,6 +1,7 @@
 import cv2
 import math
 import numpy as np
+import serial
 
 capture = cv2.VideoCapture(0)
 W_View_size = 320
@@ -10,8 +11,21 @@ capture.set(3, W_View_size)
 capture.set(4, H_View_size)
 capture.set(5, FPS)
 direction = ''
-
+BPS=4800
 yellow_range = [(15, 100, 0), (45, 455, 455)]
+
+def TX_data_py2(ser, one_byte):  # one_byte= 0~255
+    print("hjk",one_byte)
+    #ser.write(chr(int(one_byte)))          #python2.7
+    ser.write(serial.to_bytes([one_byte]))  #python3
+
+def RX_data(ser):
+    if ser.inWaiting() > 0:
+        result = ser.read(1)
+        RX = ord(result)
+        return RX
+    else:
+        return 0
 
 
 def getDegree(p1, p2):
@@ -30,40 +44,72 @@ def getSubDegree(deg1, deg2):
     else:
         return ang1
 
+serial_port = serial.Serial('/dev/ttyS0', BPS, timeout=0.01)
+TX_data_py2(serial_port, 128)
+
+def linetracing(direction):
+    if float(direction)<=-10:
+        return 162
+    elif float(direction)>=10:
+        return 161
+    else:
+        if cx<=120:
+            return 163
+        elif cx>=200:
+            return 164
+        else:
+            return 160
+
+
+
 
 while True:
     input = cv2.waitKey(1)
     _, img = capture.read()
     res = img.copy()
-    mask = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(mask, yellow_range[0], yellow_range[1])
-    img = cv2.bitwise_and(img, img, mask=mask)
-    contours, _ = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
-    if len(contours) > 0:
-        c = max(contours, key=cv2.contourArea)
-        M = cv2.moments(c)
-        if M["m00"] != 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            rows, cols = img.shape[:2]
-            [vx, vy, x, y] = cv2.fitLine(c, cv2.DIST_L2, 0, 0.01, 0.01)
-            try:
-                y1 = int((-x*vy/vx)+y)
-                y2 = int(((cols-x)*vy/vx)+y)
-                cv2.circle(img, (cx, cy), 5, (255, 255, 255), -1)
-                cv2.line(img, (cx, cy-500), (cx, cy+500), (0, 0, 255), 3)
-                cv2.line(img, (0, y1), (cols-1, y2), (0, 0, 255), 2)
-                degMid = 90
-                deg = getDegree((0, y1), (cols-1, y2))
-                resultDeg = getSubDegree(degMid, deg)
-                resultDeg = round(resultDeg, 1)
-                if deg > 0:
-                    direction = '+'+str(resultDeg)
-                else:
-                    direction = '-'+str(resultDeg)
-                cv2.drawContours(res, c, -1, (0, 255, 0), 2)
-            except Exception as e:
-                print(e)
-    if direction != '':
-        res = cv2.putText(res, direction, (0, 50), 0, 1, (0, 255, 0), 2)
+    tmp=RX_data(serial_port)
+    print(tmp)
+    if tmp==150:
+        mask = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(mask, yellow_range[0], yellow_range[1])
+        img = cv2.bitwise_and(img, img, mask=mask)
+        contours, _ = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
+        if len(contours) > 0:
+            c = max(contours, key=cv2.contourArea)
+            M = cv2.moments(c)
+            if M["m00"] != 0:
+                cx = int(M['m10']/M['m00'])  
+                cy = int(M['m01']/M['m00'])  
+                rows, cols = img.shape[:2]
+                [vx, vy, x, y] = cv2.fitLine(c, cv2.DIST_L2, 0, 0.01, 0.01)
+                try:
+                    y1 = int((-x*vy/vx)+y)
+                    y2 = int(((cols-x)*vy/vx)+y)
+                    cv2.circle(res, (cx, cy), 5, (255, 255, 255), -1)
+               
+                    cv2.line(img, (0, y1), (cols-1, y2), (0, 0, 255), 2)
+                
+                    degMid = 90
+                    deg = getDegree((0, y1), (cols-1, y2))
+                    resultDeg = getSubDegree(degMid, deg)
+                    resultDeg = round(resultDeg, 1)
+                    if deg > 0:
+                        direction = '+'+str(resultDeg)
+                    else:
+                        direction = '-'+str(resultDeg)
+                    cv2.drawContours(res, c, -1, (0, 255, 0), 2)
+                except Exception as e:
+                    print(e)
+    
+        serial_port = serial.Serial('/dev/ttyS0', BPS, timeout=0.01)
+
+        if direction != '':
+            res = cv2.putText(res, direction, (0, 50), 0, 1, (0, 255, 0), 2)
+            TX_data_py2(serial_port, linetracing(direction))
+        else:
+            TX_data_py2(serial_port, 159)
+        
+    else:
+        pass
+        
     cv2.imshow("camera", res)
