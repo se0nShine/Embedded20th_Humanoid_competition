8a087e957adb62947e500c3cdd0a15d0cac8a0c8
YoungjinNoh <youngjin_noh@naver.com>
2022-09-03 13:24:05 +0900

    재선코드 성욱코드 병합

--- detectAlpha+detectline(used).py
+++ detectAlpha+detectline(used).py
@@ -10,18 +10,19 @@ from PIL import Image
 from util import *
 
 
-
 direction = ''
-BPS=4800
+BPS = 4800
 yellow_range = [(15, 100, 0), (45, 455, 455)]
 
 alpha = ''
 color = ''
 
+
 def TX_data_py2(ser, one_byte):  # one_byte= 0~255
-    print("hjk",one_byte)
-    #ser.write(chr(int(one_byte)))          #python2.7
-    ser.write(serial.to_bytes([one_byte]))  #python3
+    print("hjk", one_byte)
+    # ser.write(chr(int(one_byte)))          #python2.7
+    ser.write(serial.to_bytes([one_byte]))  # python3
+
 
 def RX_data(ser):
     if ser.inWaiting() > 0:
@@ -31,9 +32,11 @@ def RX_data(ser):
     else:
         return 0
 
+
 serial_port = serial.Serial('/dev/ttyS0', BPS, timeout=0.01)
 TX_data_py2(serial_port, 128)
-head_rotate=170
+head_rotate = 170
+
 
 def getDegree(p1, p2):
     rad = math.atan(float(p2[1]-p1[1])/(p2[0]-p1[0]))
@@ -51,18 +54,20 @@ def getSubDegree(deg1, deg2):
     else:
         return ang1
 
+
 def linetracing(direction):
-    if float(direction)<=-10:
+    if float(direction) <= -10:
         return 162
-    elif float(direction)>=10:
+    elif float(direction) >= 10:
         return 161
     else:
-        if cx<=120:
+        if cx <= 120:
             return 163
-        elif cx>=200:
+        elif cx >= 200:
             return 164
         else:
             return 160
+
 
 def detectAlpha(img):
     global alpha
@@ -85,6 +90,7 @@ def detectAlpha(img):
     except:
         alpha = ''
 
+
 def getMessage(alpha, color):
     message = ''
     roomAlphas = ['A', 'B', 'C', 'D']
@@ -96,28 +102,62 @@ def getMessage(alpha, color):
         message = 'Shout "{}"'.format(direction[alpha])
     return message
 
+
 def detectDirection(alpha):
     if alpha == 'E':
-        TX_data_py2(serial_port,140) 
-    elif alpha =='W':
-        TX_data_py2(serial_port,141)
-    elif alpha=='S':
-        TX_data_py2(serial_port,142)
-    elif alpha=='N':
-        TX_data_py2(serial_port,143)
+        TX_data_py2(serial_port, 140)
+    elif alpha == 'W':
+        TX_data_py2(serial_port, 141)
+    elif alpha == 'S':
+        TX_data_py2(serial_port, 142)
+    elif alpha == 'N':
+        TX_data_py2(serial_port, 143)
     else:
-        TX_data_py2(serial_port,151)
+        TX_data_py2(serial_port, 151)
 
+
+def detectArrow(img):
+    global direction
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img = cv2.copyMakeBorder(
+        img, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+
+    img = cv2.Canny(img, 200, 200)
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi/180.0, threshold=30)
+
+    if lines is None:
+        direction = ''
+        return
+    temp = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        temp.append(line[0])
+    x1, y1, x2, y2 = min(temp, key=lambda l: min(l[1], l[3]))
+
+    ang1 = math.atan(float(y2-y1)/(x2-x1))
+
+    if (ang1 > np.pi/8 and ang1 < 3*np.pi/8):
+        direction = 'Right'
+    elif (ang1 < -np.pi/8 and ang1 > -3*np.pi/8):
+        direction = 'Left'
+    else:
+        direction = ''
+
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3, cv2.LINE_AA)
+    if direction != '':
+        cv2.imshow("capture", img)
 
 
 while True:
     input = cv2.waitKey(1)
     _, img = capture.read()
     res = img.copy()
-    tmp=RX_data(serial_port)
+    tmp = RX_data(serial_port)
     print(tmp)
-    
-    if tmp==150:
+
+    if tmp == 150:
         mask = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(mask, yellow_range[0], yellow_range[1])
         img = cv2.bitwise_and(img, img, mask=mask)
@@ -126,17 +166,17 @@ while True:
             c = max(contours, key=cv2.contourArea)
             M = cv2.moments(c)
             if M["m00"] != 0:
-                cx = int(M['m10']/M['m00'])  
-                cy = int(M['m01']/M['m00'])  
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
                 rows, cols = img.shape[:2]
                 [vx, vy, x, y] = cv2.fitLine(c, cv2.DIST_L2, 0, 0.01, 0.01)
                 try:
                     y1 = int((-x*vy/vx)+y)
                     y2 = int(((cols-x)*vy/vx)+y)
                     cv2.circle(res, (cx, cy), 5, (255, 255, 255), -1)
-               
+
                     cv2.line(img, (0, y1), (cols-1, y2), (0, 0, 255), 2)
-                
+
                     degMid = 90
                     deg = getDegree((0, y1), (cols-1, y2))
                     resultDeg = getSubDegree(degMid, deg)
@@ -153,10 +193,10 @@ while True:
             TX_data_py2(serial_port, linetracing(direction))
         else:
             TX_data_py2(serial_port, 159)
-            
-    elif tmp==151:
+
+    elif tmp == 151:
         TX_data_py2(serial_port, head_rotate)
-        rect= getBlackObject(img)
+        rect = getBlackObject(img)
 
         if rect['w'] > 0:
             img_alpha = cv2.getRectSubPix(img, (int(rect['w']), int(
@@ -165,19 +205,27 @@ while True:
             detectAlpha(img_alpha)
             color = 'Black'
             detectDirection(alpha)
-        
+
         if alpha != '' and color != '':
             img = drawText(img, 1, 'Text: '+alpha)
             img = drawText(img, 2, 'Color: '+color)
-        head_rotate=head_rotate+1
-        
+        head_rotate = head_rotate+1
+    elif tmp == 152:
+        _, img = capture.read()
+        img_focus = cv2.getRectSubPix(
+            img, (focus['w'], focus['h']), (focus['cx'], focus['cy']))
+        rect = getBlackObject(img_focus)
 
-        
-        
-    else:
-        pass
-    
+        if rect['w'] > 0:
+            img_arrow = cv2.getRectSubPix(img_focus, (int(rect['w']), int(
+                rect['h'])), (int(rect['cx']), int(rect['cy'])))
+            img = drawRects(img, [rect])
+            detectArrow(img_arrow)
+
+        if direction != '':
+            img = drawText(img, 1, 'Move '+direction)
+
     cv2.imshow("camera", res)
     cv2.rectangle(img, (focus['x'], focus['y']), (focus['x']+focus['w'], focus['y']+focus['h']),
-                  (255, 255, 255), 2)  
+                  (255, 255, 255), 2)
     cv2.imshow("camera", img)
