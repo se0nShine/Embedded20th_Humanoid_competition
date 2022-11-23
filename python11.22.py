836610c2ba4d943b7f1989790d2c771b9bf29adf
se0nShine <58286830+se0nShine@users.noreply.github.com>
2022-11-23 13:58:54 +0900

    Update python11.22.py

--- python11.22.py
+++ python11.22.py
@@ -529,16 +529,13 @@ def detectRoomName(img):
     cv2.imshow("ss", img_blue)
 
     ret = detectRoomWord(img_red)
-
+    roomColor = 1
+    
     if ret == 149:
-        roomColor = 1
         ret = detectRoomWord(img_blue)
         if ret != 149:
             roomColor = 2
-            ret += 4
-        return ret
     return ret
-
 
 def onChangeHMin(val):
     global lower_color
